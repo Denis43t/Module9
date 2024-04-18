fdf5a2f56004ff6ad24c25b4497b29cd9b38181d
Denis43t <javadenisstudy@gmail.com>
2024-04-18 21:32:03 +0200

    debug code
    completed grow method

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -6,42 +6,45 @@ public class MyArrayList<T> {
 
     private T[] array;
     private int size;
-    private int index=0;
-//initialization array
+    private int index = 0;
+
+    //initialization array
     public MyArrayList(int size) {
-        @SuppressWarnings("unchecked")
-        final T[] tempArray = (T[]) new Object[size];
+        @SuppressWarnings("unchecked") final T[] tempArray = (T[]) new Object[size];
         this.array = tempArray;
-        this.size=size;
-    }
-    private T[] grow(){
-        if(size<=index){
-            size*=2;
-            return array=Arrays.copyOf(this.array,size);
-        }
-        else{
-            return array;
-        }
-    }
-    public void get(int index){
-        System.out.println(array[index]);
-    }
-     public void add(T value) {
-        array[index]=value;
-        index++;
+        this.size = size;
     }
 
-    public void remove(int index){
-        for (int i = index+1; i < array.length-1; i++) {
-            array[i-1] = array[i];
+    private void grow() {
+        size *= 2;
+        array = Arrays.copyOf(this.array, size);
+    }
+
+    public void get(int index) {
+        System.out.println(array[index]);
+    }
+
+    public void add(T value) {
+        if (size <= index) {
+            grow();
+            array[index] = value;
+            index++;
+        } else {
+            array[index] = value;
+            index++;
+        }
+    }
+
+    public void remove(int index) {
+        for (int i = index + 1; i < array.length - 1; i++) {
+            array[i - 1] = array[i];
             array[i] = null;
             this.index--;
         }
     }
 
-    public void clear(){
-        @SuppressWarnings("unchecked")
-        final T[] tempArray = (T[]) new Object[size];
+    public void clear() {
+        @SuppressWarnings("unchecked") final T[] tempArray = (T[]) new Object[size];
         this.array = tempArray;
     }
 }
